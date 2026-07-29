204412a21e64d2afe9545ad247ca46a4222bf49f
PabloAnimaSola <118060579+PabloAnimaSola@users.noreply.github.com>
2026-07-29 13:59:36 +0200

    Cerrado el scanner

--- src/Main.java
+++ src/Main.java
@@ -33,6 +33,8 @@ public class Main {
         System.out.println("Ahora indica el segundo.");
         float num2 = s.nextFloat();
         float resultado = 0;
+        // Cerramos el scanner
+        s.close();
 
         switch (Integer.parseInt(operacion)) {
             case 1:
